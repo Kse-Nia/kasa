2cc834084225593ee7ef52deb52048dfec167c72
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2024-03-07 19:22:19 +0100

    update css

--- kasa-app/src/index.js
+++ kasa-app/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./style/main.css";
+//import "./style/main.css";
+import "./style/main2.css";
+//import "./index.css";
 
 import AppLayout from "./Utils/Outlet";
 
