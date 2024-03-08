c5118432adb2288c67d80fa1544e0422d92c5d7c
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2024-03-08 16:01:09 +0100

    fetch data ok

--- kasa-app/src/pages/House.jsx
+++ kasa-app/src/pages/House.jsx
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom"; // hook
+import IconOpen from "../assets/row.svg";
+import IconClose from "../assets/row2.svg";
 
 const House = (props) => {
     const { id } = useParams();
     const [house, setHouse] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
+
+    // Close/Open modal
+    const [openDescription, setOpenDescription] = useState(false);
+    const [openEquipment, setOpenEquipment] = useState(false);
+    
+    const toggleModalDescription = () => {
+        setOpenDescription(!openDescription);
+      };
+    
+      const toggleModalEquipments = () => {
+        setOpenEquipment(!openEquipment);
+      };
   
+    // Fetch data
     useEffect(() => {
       const fetchHouses = async () => {
         try {
@@ -30,39 +45,62 @@ const House = (props) => {
       fetchHouses();
     }, [id]);
   
-    if (isLoading) return <div>Chargement en cours...</div>;
-    if (error) return <div>Erreur : {error}</div>;
-    if (!house) return <div>Aucune maison n'a été trouvée</div>;
+    if (isLoading) return <p>Chargement en cours...</p>;
+    if (!house) return <p>Aucune maison n'a été trouvée</p>;
+    if (error) return <p>Erreur : {error}</p>;
 
   return (
     <div className="main">
-     <div className="info">
-     <img className="house cover" src={house.cover} alt="cover" />
-        <h1>{house.title}</h1>
-        <h2>{house.location}</h2>
-        <div className="host">
-          <h3>Hosted by {house.host.name}</h3>
-          <img className="host_picture" src={house.host.picture} alt="host" />
-        </div>
-        <div className="rating">{house.rating}</div>
-        <div className="tags">{house.tags}</div>
+    <div className="house">
+      <img className="house_cover" src={house.cover} alt="cover" />
+      <h1 className="house_title">{house.title}</h1>
+      <h2>{house.location}</h2>
+      <div className="house_host">
+        <h3 className="house_host_name">{house.host.name}</h3>
+        <img className="house_host_picture" src={house.host.picture} alt="host" />
       </div>
-      <div className="description_container">
-        <div className="description_title">
-          <p>Description</p>
-        </div>
-        <div>
-          <p>{house.description}</p>
-        </div>
-        <div className="description_equipements">
-          <p>Equipements</p>
-        </div>
-        <div>
-          <p>{house.equipments}</p>
-        </div>
-      </div> 
+      <div className="house_rating">{house.rating}</div>
+      <div className="house_tags">{house.tags}</div>
     </div>
-  );
+    <div className="house_description-container">
+      <div className="house_description-container_title" onClick={toggleModalDescription}>
+        <p>Description</p>
+        <img
+          className="house_description-container_icon"
+          src={openDescription ? IconClose : IconOpen}
+          alt="icon"
+          onClick={() => setOpenDescription((prev) => !prev)}
+        />
+      </div>
+      {openDescription && (
+        <div className="house_modal">
+          <div className="house_description-container_title">
+          </div>
+          <div>
+            <p>{house.description}</p>
+          </div>
+        </div>
+      )}
+
+      <div className="house_description-container_title" onClick={toggleModalEquipments}>
+        <p>Equipement</p>
+        <img
+          className="house_description-container_icon"
+          src={openDescription ? IconClose : IconOpen}
+          alt="icon"
+          onClick={() => setOpenEquipment((prev) => !prev)}
+        />
+      </div>
+      {openEquipment && (
+        <div className="house_modal">
+          <div>
+            <p>{house.equipments.join(', ')}</p>
+          </div>
+        </div>
+      )}
+    </div> 
+  </div>
+);
 };
 
 export default House;
