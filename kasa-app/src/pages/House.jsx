bd7f2bfaf06b80f470c8402d3ae2f35ff8d8c8e8
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2024-03-09 17:48:08 +0100

    progression desktop version

--- kasa-app/src/pages/House.jsx
+++ kasa-app/src/pages/House.jsx
@@ -1,106 +1,141 @@
 import React, { useState, useEffect } from "react";
+import Tags from "../components/Tags";
+import HouseRating from "../components/HouseRating";
 import { useLocation, useParams } from "react-router-dom"; // hook
 import IconOpen from "../assets/row.svg";
 import IconClose from "../assets/row2.svg";
 
-const House = (props) => {
-    const { id } = useParams();
-    const [house, setHouse] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+/* import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as fullStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons';
+ */
 
-    // Close/Open modal
-    const [openDescription, setOpenDescription] = useState(false);
-    const [openEquipment, setOpenEquipment] = useState(false);
-    
-    const toggleModalDescription = () => {
-        setOpenDescription(!openDescription);
-      };
-    
-      const toggleModalEquipments = () => {
-        setOpenEquipment(!openEquipment);
-      };
-  
-    // Fetch data
-    useEffect(() => {
-      const fetchHouses = async () => {
-        try {
-          const res = await fetch("/logements.json");
-          if (!res.ok) {
-            throw new Error("Une erreur est survenue, impossible d'afficher la maison");
-          }
-          const houses = await res.json();
-          const houseHouse = houses.find((house) => house.id.toString() === id);
-          if (!houseHouse) {
-            throw new Error("Désolé, maison introuvable..");
-          }
-          setHouse(houseHouse);
-        } catch (err) {
-          setError(err.message);
-        } finally {
-          setIsLoading(false);
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const House = (props) => {
+  const { id } = useParams();
+  const [house, setHouse] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Close/Open modal
+  const [openDescription, setOpenDescription] = useState(false);
+  const [openEquipment, setOpenEquipment] = useState(false);
+
+  const toggleModalDescription = () => {
+    setOpenDescription(!openDescription);
+  };
+
+  const toggleModalEquipments = () => {
+    setOpenEquipment(!openEquipment);
+  };
+
+  // Fetch data
+  useEffect(() => {
+    const fetchHouses = async () => {
+      try {
+        const res = await fetch("/logements.json");
+        if (!res.ok) {
+          throw new Error(
+            "Une erreur est survenue, impossible d'afficher la maison"
+          );
         }
-      };
-  
-      fetchHouses();
-    }, [id]);
-  
-    if (isLoading) return <p>Chargement en cours...</p>;
-    if (!house) return <p>Aucune maison n'a été trouvée</p>;
-    if (error) return <p>Erreur : {error}</p>;
+        const houses = await res.json();
+        const houseHouse = houses.find((house) => house.id.toString() === id);
+        if (!houseHouse) {
+          throw new Error("Désolé, maison introuvable..");
+        }
+        setHouse(houseHouse);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchHouses();
+  }, [id]);
+
+  if (isLoading) return <p>Chargement en cours...</p>;
+  if (!house) return <p>Aucune maison n'a été trouvée</p>; // redir error
+  if (error) return <p>Erreur : {error}</p>;
 
   return (
     <div className="main">
-    <div className="house">
-      <img className="house_cover" src={house.cover} alt="cover" />
-      <h1 className="house_title">{house.title}</h1>
-      <h2>{house.location}</h2>
-      <div className="house_host">
-        <h3 className="house_host_name">{house.host.name}</h3>
-        <img className="house_host_picture" src={house.host.picture} alt="host" />
-      </div>
-      <div className="house_rating">{house.rating}</div>
-      <div className="house_tags">{house.tags}</div>
-    </div>
-    <div className="house_description-container">
-      <div className="house_description-container_title" onClick={toggleModalDescription}>
-        <p>Description</p>
-        <img
-          className="house_description-container_icon"
-          src={openDescription ? IconClose : IconOpen}
-          alt="icon"
-          onClick={() => setOpenDescription((prev) => !prev)}
-        />
-      </div>
-      {openDescription && (
-        <div className="house_modal">
-          <div className="house_description-container_title">
+      <div className="house">
+        <img className="house_cover" src={house.cover} alt="cover" />
+        <div className="house__content">
+          <div className="house_info">
+            <h1 className="house_title">{house.title}</h1>
+            <h2 className="house_location">{house.location}</h2>
+            <div className="house_alltags">
+              {house.tags.map((tags) => {
+                return <Tags key={tags} tags={tags} />;
+              })}
+            </div>
           </div>
-          <div>
-            <p>{house.description}</p>
-          </div>
-        </div>
-      )}
 
-      <div className="house_description-container_title" onClick={toggleModalEquipments}>
-        <p>Equipement</p>
-        <img
-          className="house_description-container_icon"
-          src={openDescription ? IconClose : IconOpen}
-          alt="icon"
-          onClick={() => setOpenEquipment((prev) => !prev)}
-        />
-      </div>
-      {openEquipment && (
-        <div className="house_modal">
-          <div>
-            <p>{house.equipments.join(', ')}</p>
+          <div className="house_host_info">
+            <div className="house_host">
+              <div className="house_host_container">
+                <h3 className="house_host_name">{house.host.name}</h3>
+                <img
+                  className="house_host_picture"
+                  src={house.host.picture}
+                  alt="host"
+                />
+              </div>
+              <HouseRating rating={house.rating} />
+            </div>
           </div>
         </div>
-      )}
-    </div> 
-  </div>
-);
+      </div>
+      <div className="house_description-container">
+        <div
+          className="house_description-container_title"
+          onClick={toggleModalDescription}
+        >
+          <div className="modal_title">
+            <h2 className="modal_type">Description</h2>
+            <img
+              src={openDescription ? IconOpen : IconClose}
+              alt="icon"
+              onClick={() => setOpenDescription((prev) => !prev)}
+            />
+          </div>
+        </div>
+        {openDescription && (
+          <div className="house_modal">
+            <div className="house_description-container_title"></div>
+            <div>
+              <p>{house.description}</p>
+            </div>
+          </div>
+        )}
+
+        <div
+          className="house_description-container_title"
+          onClick={toggleModalEquipments}
+        >
+          <div className="modal_title">
+            <h2 className="modal_type">Equipment</h2>
+            <img
+              src={openDescription ? IconOpen : IconClose}
+              alt="icon"
+              onClick={() => setOpenEquipment((prev) => !prev)}
+            />
+          </div>
+        </div>
+        {openEquipment && (
+          <div className="house_modal">
+            <div>
+              <p>{house.equipments.join(", ")}</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default House;
