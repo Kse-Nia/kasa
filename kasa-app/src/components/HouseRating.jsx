c6086f7c31eb1812fe94eb0f68ddc3865d492242
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2024-03-09 16:25:25 +0100

    update sass; fix rate display

---
+++ kasa-app/src/components/HouseRating.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
+
+const HouseRating = (props) => {
+  const stars = [];
+  const rating = props.props;
+  console.log("props", rating);
+
+  for (let i = 1; i <= 5; i++) {
+    stars.push(
+      <FontAwesomeIcon
+        key={i}
+       icon={solidStar}
+        className={i <= rating ? "house_rating-on" : "house_rating-off"}
+      />
+    );
+  }
+
+  return <div className="house_rating">{stars}</div>;
+};
+
+export default HouseRating;
