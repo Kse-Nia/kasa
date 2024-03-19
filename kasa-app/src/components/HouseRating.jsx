562c26ea2260cfd2d21a2a602c66e7b1aadbe6ed
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2024-03-19 15:38:28 +0100

    fix rating props

--- kasa-app/src/components/HouseRating.jsx
+++ kasa-app/src/components/HouseRating.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as solidStar } from "@fortawesome/free-solid-svg-icons";
 
-const HouseRating = (props) => {
+const HouseRating = ({rating}) => {
   const stars = [];
-  const rating = props.props;
 
   for (let i = 1; i <= 5; i++) {
     stars.push(
       <FontAwesomeIcon
         key={i}
-       icon={solidStar}
+        icon={solidStar}
         className={i <= rating ? "house_rating-on" : "house_rating-off"}
       />
     );
