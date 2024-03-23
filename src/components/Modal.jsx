22e4cd5accb61b7cad92e335cf0d5a2cc6fc2d2e
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2024-03-23 22:47:59 +0100

    fix animations

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,19 +2,28 @@ import React, { useState } from "react";
 import IconOpen from "../assets/row.svg";
 import IconClose from "../assets/row2.svg";
 
+// new icons
+import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Modal({ title, content, isOpen, onToggle, className }) {
+  const [clicked, setClicked] = useState(false);
+ // const [closed, setClosed] = useState(false);
+
   return (
     <div className={`modal ${className}`}>
       <div className="modal_title">
         <h1 className="modal_title--name">{title}</h1>
-        <img
-            className="modal_title--icon"
-          src={isOpen ? IconClose : IconOpen}
-          alt="icon"
-          onClick={onToggle}
+        <FontAwesomeIcon
+          className={`modal_title--icon ${clicked ? "rotation" : ""}`}
+          icon={faChevronUp}
+          onClick={()=> {
+            onToggle();
+            setClicked(!clicked);
+          }}
         />
       </div>
-      {isOpen && <div className="modal_text open">{content}</div>}
+      {isOpen && <div className={`modal_text ${isOpen ? "open" : "close"}`}>{content}</div>}
     </div>
   );
 }
