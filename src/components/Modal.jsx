7fb3133bc88ab62e7aae6d78c56d3721b0b9689b
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2024-03-28 17:20:32 +0100

    fix icon animation and position

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,29 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // new icons
-import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Modal({ title, content, isOpen, onToggle, className }) {
-  const [isAnimating, setIsAnimating] = useState(false);
   const [iconDirection, setIconDirection] = useState("up");
 
-  const handleClick = () => {
-    setIsAnimating(true);
-    onToggle();
-    setIconDirection(isOpen ? "down" : "up");
-  };
+  useEffect(() => {
+    setIconDirection(isOpen ? "up" : "down");
+  }, [isOpen]);
 
   return (
     <div className={`modal ${className}`}>
       <div className="modal_title">
         <h1 className="modal_title--name">{title}</h1>
         <FontAwesomeIcon
-          className={`modal_title--icon ${
-            isAnimating ? `rotate-${iconDirection}` : ""
-          }`}
-          icon={iconDirection === "up" ? faChevronUp : faChevronDown} // Choose icon based on direction
-          onClick={handleClick}
+          className={`modal_title--icon rotate-${iconDirection}`}
+          icon={faChevronUp}
+          onClick={onToggle}
         />
       </div>
       {isOpen && (
